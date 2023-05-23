3ff0430d1c0378a0ca7bdf0428246b9fc5e2b2c8
John Lee <pushinco@gmail.com>
2023-05-23 15:46:11 +0800

    first 2023/0523

---
+++ src/main/java/com/example/demo/JohnleepersonalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JohnleepersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JohnleepersonalApplication.class, args);
+	}
+
+}
